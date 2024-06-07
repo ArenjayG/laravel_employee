7311e1f24ccdfbd8c0d1244988015706445a9dd0
ArenjayG <alejandrojay77@gmail.com>
2024-06-07 15:10:31 +0800

    hello

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\employeemanagement;
+
+
+
+Route::get('employees', [employeemanagement::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,8 +18,13 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::middleware('auth')->group(function () {
     Route::view('about', 'about')->name('about');
 
-    Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
-
+    Route::get('employee/{id}/edit', [employeemanagement::class, 'edit'])->name('employee.edit');
+    Route::patch('employee/{id}', [employeemanagement::class, 'update'])->name('employee.update');
+    Route::delete('employee/{id}', [employeemanagement::class, 'destroy'])->name('employee.destroy');
+    Route::get('/employee', [employeemanagement::class, 'index'])->name('employee.index');
+    Route::post('/employee', [employeemanagement::class, 'store'])->name('employee.store');
+    
+    
     Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
     Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
 });
